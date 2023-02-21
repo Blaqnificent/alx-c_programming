b952e78de1c838c7f8f099d5b7f97f827ae21719
Blaqnificent <bayoduduyemi@gmail.com>
2023-02-21 13:28:24 -0800

    Testing

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *main - Print '_putchar', followed by a new line
+ *Return: 0
+ */
+int main(void)
+{	char text[8] = "_putchar";
+	for (int i = 0; i < 8; ++i)
+	{
+		_putchar(text[i]);
+	}
+	_putchar("\n");
+	return (0);
+}
