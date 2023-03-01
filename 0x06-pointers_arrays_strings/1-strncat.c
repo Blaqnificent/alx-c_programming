457585bc59b5615b6bf225521d79a5dcf1b9adfe
Blaqnificent <bayoduduyemi@gmail.com>
2023-03-01 13:16:45 -0800

    Test

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,16 +10,19 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	while (*dest)
-		dest++;
-	for (i = 0; i < n; i++)
+	int i = 0;
+	int j = 0;
+
+	while (dest[i] != '\0') /*i is index of null terminator*/
+		i++;
+
+	while (src[j] != src[n]) /*append replacing null terminator*/
 	{
-		while (*src)
-		{
-			*dest = *src;
-			dest++;
-			src++;
-		}
-		*dest = '\0';
+		dest[i] = src[j];
+		i++;
+		j++;
 	}
+	dest[i] = '\0';
+
+	return (dest);
 }
