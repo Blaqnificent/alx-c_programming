0b9f7ee5e76ee861fc43e28f03c9f1275c845418
Blaqnificent <bayoduduyemi@gmail.com>
2023-03-01 13:13:17 -0800

    Test

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,16 +8,19 @@
 
 char *_strcat(char *dest, char *src)
 {
-        int i = 0;
+	int i = 0;
 	int j = 0;
-	while (*(dest + i))
-	   i++;
-	while (*(src + j))
+
+	while (*(dest  +  i))
 	{
-	   *(dest+i) = *(src+j);
-	   i++;
-	   j++;
+		i++;
 	}
-	*(dest+i) = '\0';
+	while (*(src  +  j))
+	{
+		*(dest  +  i) = *(src  +  j);
+		i++;
+		j++;
+	}
+	*(dest  +  i) = '\0';
 	return (dest);
 }
