3755362c59525d2379612ca16f4b6566bc1e4733
Blaqnificent <bayoduduyemi@gmail.com>
2023-03-20 18:30:13 -0700

    Testing again

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,16 +7,19 @@
  * @age: second member
  * @owner: third member
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
 
+
+typedef dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-typedef struct dog dog_t;
+
 #endif
