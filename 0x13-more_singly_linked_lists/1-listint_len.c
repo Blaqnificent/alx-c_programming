8ea5c4c5f74128b30cc08be2b2b05ea6d83950d1
Blaqnificent <bayoduduyemi@gmail.com>
2023-04-04 17:44:01 -0700

    More tests

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * listint_len - Prints the length of a listint_t linked list
+ * @h: linked list of listint_t type
+ * Return: Length of the given linkedlist
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h = n->next;
+	}
+	return (count)
+}
